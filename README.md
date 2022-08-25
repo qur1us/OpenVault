64de75d966d1e1c0dfacc17ab3b4e96652ef8a5e
UjoQurius <patrik.pis@gmail.com>
2022-08-25 20:20:17 +0200

    Source upload

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # OpenVault
+[Obsidian](http://obsidian.md) is a awesome tool for note taking and creating a so called "second brain". However, it lacks a bit of a vault management and may be a bit frustrating to launch a particular vault at times (especially if like me, you change the vaults frequently). This app solves the issue.
+
+If you want to have the obsidian on the taskbar and at the same time to have the option to choose the vault, with a right click on the app while pinned on the taskbar, you can open a desired vault.
+
+The application is not ready to go, you will have to modify the source code according to your preference and vaults. This ought to server as a template of some sort.
+
+# How to install
+1. Compile the source in Visual Studio
+2. Add the app to the taskbar
+3. Run the app for the first time (nothing is supposed to happen)
+4. You can now right click the app and run any vault
+
+<br>
+
+If you have any questions, contact me on [Twitter](https://twitter.com/_qurius)
---
+++ src/App.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Shell;
+
+namespace OpenVault
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            JumpList jl = new JumpList();
+
+            // Personal
+            JumpTask personalVault = new JumpTask();
+            personalVault.ApplicationPath = "obsidian://open/?vault=Personal";
+            personalVault.Title = "Personal";
+
+            // School
+            JumpTask schoolVault = new JumpTask();
+            schoolVault.ApplicationPath = "obsidian://open/?vault=school";
+            schoolVault.Title = "School";
+
+            // Infosec
+            JumpTask infosecVault = new JumpTask();
+            infosecVault.ApplicationPath = "obsidian://open/?vault=Information Security";
+            infosecVault.Title = "Information Security";
+
+            // EOSec
+            JumpTask eosecVault = new JumpTask();
+            eosecVault.ApplicationPath = "obsidian://open/?vault=EO Security";
+            eosecVault.Title = "EO Security";
+
+
+            jl.JumpItems.Add(personalVault);
+            jl.JumpItems.Add(schoolVault);
+            jl.JumpItems.Add(infosecVault);
+            jl.JumpItems.Add(eosecVault);
+
+            JumpList.SetJumpList(App.Current, jl);
+
+            App.Current.Shutdown();
+        }
+    }
+}
