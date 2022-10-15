2d2b88990e22fa19bf40f128ded391575ab79500
UjoQurius <patrik.pis@gmail.com>
2022-10-15 09:36:49 +0200

    Added system tray support

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Shell;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Application = System.Windows.Application;
 
 namespace OpenVault
 {
@@ -12,6 +17,7 @@ namespace OpenVault
         {
             base.OnStartup(e);
 
+            // Rightclick on the app added to the taskbar
             JumpList jl = new JumpList();
 
             // Personal
@@ -19,30 +25,84 @@ namespace OpenVault
             personalVault.ApplicationPath = "obsidian://open/?vault=Personal";
             personalVault.Title = "Personal";
 
-            // School
-            JumpTask schoolVault = new JumpTask();
-            schoolVault.ApplicationPath = "obsidian://open/?vault=school";
-            schoolVault.Title = "School";
+            // EOSec
+            JumpTask eosecVault = new JumpTask();
+            eosecVault.ApplicationPath = "obsidian://open/?vault=EO Security";
+            eosecVault.IconResourcePath = "..\\Resources\\EO_logo.ico";
+            eosecVault.Title = "EO Security";
 
             // Infosec
             JumpTask infosecVault = new JumpTask();
             infosecVault.ApplicationPath = "obsidian://open/?vault=Information Security";
+            infosecVault.IconResourcePath = "..\\Resources\\htb.ico";
             infosecVault.Title = "Information Security";
 
-            // EOSec
-            JumpTask eosecVault = new JumpTask();
-            eosecVault.ApplicationPath = "obsidian://open/?vault=EO Security";
-            eosecVault.Title = "EO Security";
-
-
             jl.JumpItems.Add(personalVault);
-            jl.JumpItems.Add(schoolVault);
-            jl.JumpItems.Add(infosecVault);
             jl.JumpItems.Add(eosecVault);
-
+            jl.JumpItems.Add(infosecVault);
+            
             JumpList.SetJumpList(App.Current, jl);
 
-            App.Current.Shutdown();
+
+            // System tray
+            NotifyIcon TrayIcon = new NotifyIcon();
+            ContextMenuStrip menu = new ContextMenuStrip();
+
+            TrayIcon.Icon = new System.Drawing.Icon("..\\..\\..\\Resources\\obsidian.ico");
+            TrayIcon.ContextMenuStrip = menu;
+
+            ToolStripMenuItem personal = new ToolStripMenuItem("Personal");
+            personal.Click += new EventHandler(StartPersonalVault);
+            
+            ToolStripMenuItem infosec = new ToolStripMenuItem("EO Security");
+            infosec.Click += new EventHandler(StartWorkVault);
+            
+            ToolStripMenuItem work = new ToolStripMenuItem("Information Security");
+            work.Click += new EventHandler(StartInfosecVault);
+
+            ToolStripMenuItem exitMenu = new ToolStripMenuItem("Exit");
+            exitMenu.Click += new EventHandler(Exit);
+
+            menu.Items.Add(personal);
+            menu.Items.Add(work);
+            menu.Items.Add(infosec);
+            menu.Items.Add(exitMenu);  
+
+            TrayIcon.Visible = true;
+        }
+
+        public void StartPersonalVault(object sender, EventArgs e)
+        {
+            string url = "obsidian://open/?vault=Personal";
+
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.UseShellExecute = true;
+            info.FileName = url;
+            Process.Start(info);
+        }
+
+        public void StartWorkVault(object sender, EventArgs e)
+        {
+            string url = "obsidian://open/?vault=EO Security";
+
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.UseShellExecute = true;
+            info.FileName = url;
+            Process.Start(info);
+        }
+
+        public void StartInfosecVault(object sender, EventArgs e)
+        {
+            string url = "obsidian://open/?vault=Information Security";
+
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.UseShellExecute = true;
+            info.FileName = url;
+            Process.Start(info);
+        }
+        public void Exit(object sender, EventArgs e)
+        {
+            Current.Shutdown();
         }
     }
 }
